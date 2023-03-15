f78866f69e09c598a25d0871765935318b431bbf
jinhee2 <jinhee@mz.co.kr>
2023-03-15 16:48:39 +0900

    pc commit

---
+++ meta-pc/src/App.js
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import './styles/common.scss';
+import './styles/contents.scss';
+import './styles/lectureRoom.scss';
+import MainPage from "./pages/MainPage";
+import MyRoom from "./pages/MyRoom";
+import CounselingwaitingRoom from "./pages/CounselingwaitingRoom";
+import SpaceSet from "./pages/SpaceSet";
+import MainPage02 from "./pages/MainPage02";
+import Tb_MainPage from "./pages/Tb_MainPage";
+
+
+// import Home from "./Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/MyRoom" element={<MyRoom />} />
+      <Route path="/CounselingwaitingRoom" element={<CounselingwaitingRoom />} />
+      <Route path="/SpaceSet" element={<SpaceSet />} />
+      <Route path="/MainPage02" element={<MainPage02 />} />
+      {/* 더본 코리아*/}
+      <Route path="/Tb_MainPage" element={<Tb_MainPage/>} />
+      {/* <Route path="/Home" element={<Home />} /> */}
+
+      {/* ETC */}
+      <Route path="/" element={<MainPage />} />
+
+    </Routes>
+  );
+}
+
+export default App;
