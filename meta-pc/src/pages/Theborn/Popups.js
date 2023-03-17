705f439c2eec9a6fa83aaba66534660c7bc230b4
hjjin88~ <hjjin@mz.co.kr>
2023-03-17 10:32:46 +0900

    ㅇㅇ

--- meta-pc/src/pages/Theborn/Popups.js
+++ meta-pc/src/pages/Theborn/Popups.js
@@ -1,7 +1,10 @@
 import Quest from "./Quest";
 import Pop_ty01 from "./Pop_ty01";
 import Pop_ty02 from "./Pop_ty02";
-
+import Kiosk from "./Kiosk";
+import NpcMessage from "./NpcMessage";
+import EventMessage from "./EventMessage";
+import QuestIcon from "./QuestIcon";
 
 const Popups = ({ popName, setPopName }) => {
     return (
@@ -14,6 +17,18 @@ const Popups = ({ popName, setPopName }) => {
         )}
         { popName === 'Pop_ty02' && (
           <Pop_ty02 setPopName={setPopName} />
+        )}
+        { popName === 'Kiosk' && (
+          <Kiosk setPopName={setPopName} />
+        )}
+        { popName === 'NpcMessage' && (
+          <NpcMessage setPopName={setPopName} />
+        )}
+        { popName === 'EventMessage' && (
+          <EventMessage setPopName={setPopName} />
+        )}
+        { popName === 'QuestIcon' && (
+          <QuestIcon setPopName={setPopName} />
         )}
         </>
     )
